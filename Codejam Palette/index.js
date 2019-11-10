1042d56452c9d0f4281fa0b69a5a852822c91298
Kiryl Kubar <kirillkubar@gmail.com>
2019-11-10 20:53:25 +0300

    refactor: change code with help eslint

--- Codejam Palette/index.js
+++ Codejam Palette/index.js
@@ -1,91 +1,94 @@
 let isPencil = false;
 let isDraw = false;
-let isPaintBucket = false; 
+let isPaintBucket = false;
 let isChooseColor = false;
 let color = '#FFC107';
 let colorPrev = '';
-document.getElementById('swapPrevColor').style.background = '#FFEB3B'
+document.getElementById('swapPrevColor').style.background = '#FFEB3B';
 document.getElementById('inputColor').value = '#FFC107';
-function draw(){
-  let canvas = document.getElementById('canvas');
-  let ctx = canvas.getContext('2d');
-   if(localStorage.getItem('canvas')){
-    let dataURL = localStorage.getItem('canvas');
-    let img = new Image;
+function draw() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  if (localStorage.getItem('canvas')) {
+    const dataURL = localStorage.getItem('canvas');
+    const img = new Image();
     img.src = dataURL;
-    img.onload = function () {
+    img.onload = function load() {
       ctx.drawImage(img, 0, 0);
-  };
-  }
-  else {
+    };
+  } else {
     const getArr = async (url) => {
       try {
-        const arr = await fetch(url).then(responce => responce.json())
+        const arr = await fetch(url).then((responce) => responce.json());
         return arr;
-      } catch(err){
-        console.log(err)
+      } catch (err) {
+        return err;
       }
-    }
-    let canvasSize = 512;
-    let mas = getArr('https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json')
-    mas.then(data => {
-      for(let i = 0; i < data.length; i++){
-        for(let j = 0; j < data.length; j++){
-          let x = canvasSize / data.length * i;
-          let y = canvasSize / data.length * j;
-          let res = `#${data[i][j]}`;
+    };
+    const canvasSize = 512;
+    const mas = getArr('https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json');
+    mas.then((data) => {
+      for (let i = 0; i < data.length; i += 1) {
+        for (let j = 0; j < data.length; j += 1) {
+          const x = canvasSize / (data.length * i);
+          const y = canvasSize / (data.length * j);
+          const res = `#${data[i][j]}`;
           ctx.fillStyle = res;
           ctx.fillRect(x, y, canvasSize / data.length, canvasSize / data.length);
         }
       }
-    })
+    });
   }
 }
 
-document.getElementById('mainItems').addEventListener('click', function(event){
-  let target = event.target
-  let element = target.closest('div');
-  if(element.className === 'main__items' || element.id === 'transform')
-    return;
+function rgbToHex(str) {
+  let rgbElements = str.slice(4, str.length - 1);
+  rgbElements = rgbElements.split(', ');
+  return `#${((1 << 24) + (+rgbElements[0] << 16) + (+rgbElements[1] << 8) + +rgbElements[2]).toString(16).slice(1)}`;
+}
+
+document.getElementById('mainItems').addEventListener('click', (event) => {
+  const { target } = event;
+  const element = target.closest('div');
+  if (element.className === 'main__items' || element.id === 'transform') { return; }
   element.classList.add('active');
-  if(element.id === 'pencil'){
+  if (element.id === 'pencil') {
     isPencil = true;
     isPaintBucket = false;
     isChooseColor = false;
     document.getElementById('paintBucket').classList.remove('active');
     document.getElementById('chooseColor').classList.remove('active');
   }
-  if(element.id === 'paintBucket'){
+  if (element.id === 'paintBucket') {
     isPencil = false;
     isChooseColor = false;
     isPaintBucket = true;
     document.getElementById('chooseColor').classList.remove('active');
     document.getElementById('pencil').classList.remove('active');
   }
-  if(element.id === 'chooseColor'){
+  if (element.id === 'chooseColor') {
     isPencil = false;
     isChooseColor = true;
     isPaintBucket = false;
     document.getElementById('pencil').classList.remove('active');
     document.getElementById('paintBucket').classList.remove('active');
   }
-})
+});
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-document.getElementById('canvas').addEventListener('mousedown', function(event){
-  if(isPencil)
-  { 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+document.getElementById('canvas').addEventListener('mousedown', (event) => {
+  if (isPencil) {
     ctx.beginPath();
-    let x = event.offsetX;
-    let y = event.offsetY;
-    ctx.moveTo(x,y);
-    isDraw = true
+    const x = event.offsetX;
+    const y = event.offsetY;
+    ctx.moveTo(x, y);
+    isDraw = true;
   }
-})
+});
 
-document.getElementById('canvas').addEventListener('mousemove', function(event){
-  if(isDraw){
+document.getElementById('canvas').addEventListener('mousemove', (event) => {
+  if (isDraw) {
     // let x = event.offsetX;
     // let y = event.offsetY;
     // ctx.lineWidth = 128;
@@ -93,75 +96,66 @@ document.getElementById('canvas').addEventListener('mousemove', function(event){
     // ctx.stroke();
     let x = event.offsetX;
     let y = event.offsetY;
-    for(let i = 1; i <= 4; i++){
-      if(x < 128 * i){
+    for (let i = 1; i <= 4; i += 1) {
+      if (x < 128 * i) {
         x = 128 * (i - 1);
         break;
       }
     }
-    for(let i = 1; i <= 4; i++){
-      if(y < 128 * i){
+    for (let i = 1; i <= 4; i += 1) {
+      if (y < 128 * i) {
         y = 128 * (i - 1);
         break;
       }
     }
     ctx.fillStyle = color;
-    ctx.fillRect(x,y,128,128);
+    ctx.fillRect(x, y, 128, 128);
   }
-})
+});
 
-document.getElementById('canvas').addEventListener('mouseup', function(){
+document.getElementById('canvas').addEventListener('mouseup', () => {
   isDraw = false;
   localStorage.setItem('canvas', canvas.toDataURL());
-})
+});
 
-document.getElementById('canvas').addEventListener('mousedown',function(event){
-  if(isPencil)
-  { 
+document.getElementById('canvas').addEventListener('mousedown', (event) => {
+  if (isPencil) {
     let x = event.offsetX;
     let y = event.offsetY;
-    for(let i = 1; i <= 4; i++){
-      if(x < 128 * i){
+    for (let i = 1; i <= 4; i += 1) {
+      if (x < 128 * i) {
         x = 128 * (i - 1);
         break;
       }
     }
-    for(let i = 1; i <= 4; i++){
-      if(y < 128 * i){
+    for (let i = 1; i <= 4; i += 1) {
+      if (y < 128 * i) {
         y = 128 * (i - 1);
         break;
       }
     }
     ctx.fillStyle = color;
-    ctx.fillRect(x,y,128,128);
+    ctx.fillRect(x, y, 128, 128);
     localStorage.setItem('canvas', canvas.toDataURL());
   }
-})
+});
 
-// document.getElementById('canvas').addEventListener('click',function(){
-//   if(isPaintBucket){
-//     ctx.fillStyle = color;
-//     ctx.fillRect(0,0,512,512);
-//     localStorage.setItem('canvas', canvas.toDataURL());
-//   }
-// })
-
-document.getElementById('mainColors').addEventListener('click', function(event){
-  let target = event.target
-  let element = target.closest('div');
-  if(element.className === 'main__colors'){
+document.getElementById('mainColors').addEventListener('click', (event) => {
+  const { target } = event;
+  const element = target.closest('div');
+  if (element.className === 'main__colors') {
     return;
   }
   colorPrev = document.getElementById('swapPrevColor').style.background;
   colorPrev = rgbToHex(colorPrev);
   document.getElementById('swapPrevColor').style.background = color;
-  if(element.id == 'red' || element.className == 'circle-red'){
+  if (element.id === 'red' || element.className === 'circle-red') {
     color = '#F74141';
   }
-  if(element.id === 'blue' || element.className == 'circle-blue'){
+  if (element.id === 'blue' || element.className === 'circle-blue') {
     color = '#0088ff';
   }
-  if(element.id === 'prevColor' || element.className == 'circle-prev'){
+  if (element.id === 'prevColor' || element.className === 'circle-prev') {
     document.getElementById('swapCurrentColor').style.background = colorPrev;
     document.getElementById('inputColor').value = colorPrev;
     color = colorPrev;
@@ -169,43 +163,35 @@ document.getElementById('mainColors').addEventListener('click', function(event){
   }
   document.getElementById('swapCurrentColor').style.background = color;
   document.getElementById('inputColor').value = color;
-  color = color;
-})
+});
 
-document.getElementById('canvas').addEventListener('click', function(event){
-  if(isChooseColor){
-    let x = event.offsetX;
-    let y = event.offsetY;
-    let colorPixel = ctx.getImageData(x, y, 1, 1).data;
-    let rgb = `rgb(${colorPixel[0]}, ${colorPixel[1]}, ${colorPixel[2]})`;
+document.getElementById('canvas').addEventListener('click', (event) => {
+  if (isChooseColor) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    const colorPixel = ctx.getImageData(x, y, 1, 1).data;
+    const rgb = `rgb(${colorPixel[0]}, ${colorPixel[1]}, ${colorPixel[2]})`;
     document.getElementById('swapPrevColor').style.background = color;
     color = rgbToHex(rgb);
     document.getElementById('swapCurrentColor').style.background = color;
     document.getElementById('inputColor').value = color;
   }
-})
+});
 
-document.getElementById('inputColor').addEventListener('input',function(){
+document.getElementById('inputColor').addEventListener('input', () => {
   document.getElementById('swapPrevColor').style.background = color;
   color = document.getElementById('inputColor').value;
   document.getElementById('swapCurrentColor').style.background = color;
-})
+});
 
-
-function rgbToHex(str) {
-  str = str.slice(4,str.length-1);
-  str = str.split(', ');
-  return "#" + ((1 << 24) + (+str[0] << 16) + (+str[1] << 8) + +str[2]).toString(16).slice(1);
-}
-
-setInterval(colorNow,0);
-
-function colorNow(){
+function colorNow() {
   ctx.strokeStyle = color;
 }
 
-document.addEventListener('keydown',function(event){
-  if(event.code === 'KeyB'){
+setInterval(colorNow, 0);
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'KeyB') {
     document.getElementById('paintBucket').classList.add('active');
     isPencil = false;
     isChooseColor = false;
@@ -213,7 +199,7 @@ document.addEventListener('keydown',function(event){
     document.getElementById('pencil').classList.remove('active');
     document.getElementById('chooseColor').classList.remove('active');
   }
-  if(event.code === 'KeyC'){
+  if (event.code === 'KeyC') {
     document.getElementById('chooseColor').classList.add('active');
     isPencil = false;
     isChooseColor = true;
@@ -221,7 +207,7 @@ document.addEventListener('keydown',function(event){
     document.getElementById('paintBucket').classList.remove('active');
     document.getElementById('pencil').classList.remove('active');
   }
-  if(event.code === 'KeyP'){
+  if (event.code === 'KeyP') {
     document.getElementById('pencil').classList.add('active');
     isPencil = true;
     isChooseColor = false;
@@ -229,73 +215,73 @@ document.addEventListener('keydown',function(event){
     document.getElementById('paintBucket').classList.remove('active');
     document.getElementById('chooseColor').classList.remove('active');
   }
-})
-document.getElementById('canvas').addEventListener('click', function(event){
-  if(isPaintBucket){
+});
+document.getElementById('canvas').addEventListener('click', (event) => {
+  if (isPaintBucket) {
     let x = event.offsetX;
     let y = event.offsetY;
-    let colorPixel = ctx.getImageData(x, y, 1, 1).data;
-    let rgb = `rgb(${colorPixel[0]}, ${colorPixel[1]}, ${colorPixel[2]})`;
-    let value = rgbToHex(rgb);
-    for(let i = 1; i <= 4; i++){
-      if(x < 128 * i){
+    const colorPixel = ctx.getImageData(x, y, 1, 1).data;
+    const rgb = `rgb(${colorPixel[0]}, ${colorPixel[1]}, ${colorPixel[2]})`;
+    const value = rgbToHex(rgb);
+    for (let i = 1; i <= 4; i += 1) {
+      if (x < 128 * i) {
         x = 128 * (i - 1);
         break;
       }
     }
-    for(let i = 1; i <= 4; i++){
-      if(y < 128 * i){
+    for (let i = 1; i <= 4; i += 1) {
+      if (y < 128 * i) {
         y = 128 * (i - 1);
         break;
       }
     }
-    let pixels = [];
-    let pixelMeet = {}
-    pixelMeet[x + ' ' + y] = true;
+    const pixels = [];
+    const pixelMeet = {};
+    pixelMeet[`${x} ${y}`] = true;
     ctx.fillStyle = color;
-    ctx.fillRect(x,y,128,128);
+    ctx.fillRect(x, y, 128, 128);
 
-    if(x != 384){
-      pixels.push([x+128,y])
+    if (x !== 384) {
+      pixels.push([x + 128, y]);
     }
-    if(x != 0){
-      pixels.push([x-128,y])
+    if (x !== 0) {
+      pixels.push([x - 128, y]);
     }
-    if(y != 384){
-      pixels.push([x,y+128])
+    if (y !== 384) {
+      pixels.push([x, y + 128]);
     }
-    if(y != 0){
-      pixels.push([x,y-128])
+    if (y !== 0) {
+      pixels.push([x, y - 128]);
     }
-    while(pixels.length > 0){
-        let pixel = pixels.pop();
-        let xNow = pixel[0];
-        let yNow = pixel[1];
-        if(pixelMeet[xNow + ' ' + yNow] == true){
-          continue;
+    while (pixels.length > 0) {
+      const pixel = pixels.pop();
+      const xNow = pixel[0];
+      const yNow = pixel[1];
+      if (pixelMeet[`${xNow} ${yNow}`] === true) {
+        continue;
+      }
+      pixelMeet[`${xNow} ${yNow}`] = true;
+      const colorPixel2 = ctx.getImageData(xNow, yNow, 1, 1).data;
+      const rgb2 = `rgb(${colorPixel2[0]}, ${colorPixel2[1]}, ${colorPixel2[2]})`;
+      const value2 = rgbToHex(rgb2);
+      if (value2 === value) {
+        ctx.fillRect(xNow, yNow, 128, 128);
+        if (xNow !== 384 && pixelMeet[`${+xNow + +128} ${yNow}`] !== true) {
+          pixels.push([xNow + 128, yNow]);
         }
-        pixelMeet[xNow + ' ' + yNow] = true;
-        let colorPixel2 = ctx.getImageData(xNow, yNow, 1, 1).data;
-        let rgb2 = `rgb(${colorPixel2[0]}, ${colorPixel2[1]}, ${colorPixel2[2]})`;
-        let value2 = rgbToHex(rgb2);
-        if(value2 == value){
-          ctx.fillRect(xNow,yNow,128,128);
-          if(xNow != 384 && pixelMeet[(+xNow + +128) + ' '+ yNow] != true){
-            pixels.push([xNow+128,yNow])
-          }
-          if(xNow != 0 && pixelMeet[(+xNow - +128) + ' '+ yNow] != true){
-            pixels.push([xNow-128,yNow])
-          }
-          if(yNow != 384 && pixelMeet[xNow + ' ' + (+yNow + +128)] != true){
-            pixels.push([xNow,yNow+128])
-          }
-          if(yNow != 0 &&  pixelMeet[xNow + ' ' + (+yNow - +128)] != true){
-            pixels.push([xNow,yNow-128])
-          }
+        if (xNow !== 0 && pixelMeet[`${+xNow - +128} ${yNow}`] !== true) {
+          pixels.push([xNow - 128, yNow]);
+        }
+        if (yNow !== 384 && pixelMeet[`${xNow} ${+yNow + +128}`] !== true) {
+          pixels.push([xNow, yNow + 128]);
+        }
+        if (yNow !== 0 && pixelMeet[`${xNow} ${+yNow - +128}`] !== true) {
+          pixels.push([xNow, yNow - 128]);
         }
       }
+    }
     localStorage.setItem('canvas', canvas.toDataURL());
   }
-})
+});
 
 draw();
