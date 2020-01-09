b54211955e9f7bf5ee2feb626d21a6eef0841f05
Kiryl Kubar <kirillkubar@gmail.com>
2020-01-09 12:49:08 +0300

    add image loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,26 @@ module.exports = {
   devtool: "source-map",
   watch: true,
   mode: 'development',
+  module:{
+    rules: [
+        {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+            'file-loader',
+            {
+                loader: 'image-webpack-loader',
+                options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+                },
+            },
+        ],
+    },
+    {
+      // HTML LOADER
+      test: /\.html$/,
+      loader: 'html-loader'
+    },
+  ]
+  },
 };
